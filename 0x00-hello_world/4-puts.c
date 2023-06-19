99187d9750979f828f8f39687dae43326841676f
codingbelhaous <coding.belhaous@gmail.com>
2023-06-19 06:48:46 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+
+/** Programe that print out 'Programming is like building a multilingual puzzle'
+ * using the puts function'
+ * /
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
