7278439d0244cd4860352993fa016dda2057c396
codingbelhaous <coding.belhaous@gmail.com>
2023-06-19 12:43:57 -0700

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,11 +8,6 @@
  */
 int main(void)
 {
-char message[] = "and that piece of art is useful\"\
- - Dora Korpar, 2015-10-19\n";
-
-	size_t length = sizeof(message) - 1;
-
-	write(2, message, length);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
