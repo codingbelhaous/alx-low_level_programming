a7be83bcf31db85ccf078710edd33bc4f1f06d05
codingbelhaous <coding.belhaous@gmail.com>
2023-06-19 11:37:14 -0700

    Not printf and not puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * main - Entry
+ *
+ * Not using printf or puts functions
+ *
+ * Return: 0
+ */
+int main() {
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	size_t length = siweof(message)-1;
+	write(2, message, length);
+	return (1);
+}
