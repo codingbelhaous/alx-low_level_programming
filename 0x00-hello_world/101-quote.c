aff85e0603fd2b09152ce843a3fcb462d01c4b29
codingbelhaous <coding.belhaous@gmail.com>
2023-06-19 12:16:59 -0700

    Not printf and not puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,9 +6,13 @@
  *
  * Return: 0
  */
-int main() {
-	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	size_t length = sizeof(message)-1;
+int main(void)
+{
+	char message[] = "and that piece of art is useful\"
+		- Dora Korpar, 2015-10-19\n";
+
+	size_t length = sizeof(message) - 1;
+
 	write(2, message, length);
 	return (1);
 }
