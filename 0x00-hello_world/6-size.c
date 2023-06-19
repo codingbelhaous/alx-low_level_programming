d356d074038feba9aeb8420989ae5d9008e25adf
codingbelhaous <coding.belhaous@gmail.com>
2023-06-19 07:58:18 -0700

    Sizeof data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-	pritnf("Size of a char: %d byte(s)\n", sizeof(char));
-	pritnf("Size of an int: %d byte(s)\n", sizeof(int));
-	pritnf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	pritnf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	pritnf("Size of a float: %d byte(s)\n", sizeof(float));
+	pritnf("Size of a char: %lu byte(s)\n", sizeof(char));
+	pritnf("Size of an int: %lu byte(s)\n", sizeof(int));
+	pritnf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	pritnf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	pritnf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
