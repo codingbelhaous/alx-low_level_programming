56a2170e67e80c034e89149fc0fefdf8a6478558
codingbelhaous <coding.belhaous@gmail.com>
2023-06-21 16:13:20 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+include "main.h"
+
+/**
+ 
+ * print_sign - function that prints out the sign of the number prompt
+ *
+ * @n: number to be checkd
+ *
+ * Return: 1 if n is positive, 0 if n is equal zero -1 if it's negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar ('-');
+		return (-1);
+	}
+	_putchar('\n');
+}
