e2332058f15e42a69ef8e2a8e56ac92f3373a82e
codingbelhaous <coding.belhaous@gmail.com>
2023-06-21 16:37:32 -0700

    i+j

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*add - function that prompt two integers from the user
+*@i: 1st number to prompt
+*@j: 2nd number to prompt
+*
+*Return: result
+*/
+int add(int i, int j)
+{
+	return (i + j);
+}
