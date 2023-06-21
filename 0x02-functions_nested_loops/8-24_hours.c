d23396b7d0ceccec5395abdf78b352d8d2822c61
codingbelhaous <coding.belhaous@gmail.com>
2023-06-21 16:24:39 -0700

    hours and minutes of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function print out the minutes of the day
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int hour;
+	int min;
+
+	for (hour = 0; hour < 24; hour++)
+	for (min = 0; min < 60; min++)
+	{
+		_putchar('0' + (hour / 10));
+		_putchar('0' + (hour % 10));
+		_putchar(':');
+		_putchar('0' + (min / 10));
+		_putchar('0' + (min % 10));
+		_putchar('\n');
+	}
+}
