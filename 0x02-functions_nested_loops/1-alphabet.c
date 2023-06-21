079d58e1c11125934991ff7bb300feb892eff12f
codingbelhaous <coding.belhaous@gmail.com>
2023-06-21 14:10:49 -0700

    alphabet in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print out all the alphabet in lowercas
+ *
+ * Return: void
+ */
+void print_alphabet()
+{
+	for (i = 'a'; i <= z; a++)
+	{
+		_putchar(al);
+	}
+	_putchar('\n')
+}
