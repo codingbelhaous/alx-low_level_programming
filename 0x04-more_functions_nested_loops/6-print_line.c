4499f12b71f7ae139301a7b4492aa7ba9a4e0394
codingbelhaous <coding.belhaous@gmail.com>
2023-06-22 15:10:46 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - print out a straight line in the terminal
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 0; i = n - 1; i++)
+	{
+		_putchar('_');
+	}
+}
