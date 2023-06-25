447d868de2893c75ac083df34d77028be3279e35
codingbelhaous <coding.belhaous@gmail.com>
2023-06-25 15:37:03 -0700

    print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,27 +8,27 @@
 
 int main(void)
 {
-int i;
-int j;
-int k;
+	int a;
+	int b;
+	int c;
 
-for (i = 48; i < 56; i++)
-{
-for (j = i + 1; j < 57; j++)
-{
-for (k = j + 1; k < 58; k++)
-{
-putchar(i);
-putchar(j);
-putchar(k);
-if (i != 55 || j != 56 || k != 57)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-putchar('\n');
-return (0);
+	for (a = 48; a < 56; a++)
+	{
+		for (b = a + 1; b < 57; b++)
+		{
+			for (c = b + 1; c < 58; c++)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+				if (a != 55 || b != 56 || c != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
