fd01412904bb326169ba6b5361c65ed4100cb242
codingbelhaous <coding.belhaous@gmail.com>
2023-06-25 12:22:54 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print out the numbers of base 10 starting from 0 using int variable
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		putchar(c + '0');
+	}
+	putchar('\n');
+	return (0);
+}
