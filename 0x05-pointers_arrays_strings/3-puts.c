5712b7fe368a5427d504694dc024cc7c4ffb2dbe
codingbelhaous <coding.belhaous@gmail.com>
2023-06-26 16:21:34 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - function that print out a string
+ *
+ * @s: parameter
+ *
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+	while(s[i])
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');       
+}
