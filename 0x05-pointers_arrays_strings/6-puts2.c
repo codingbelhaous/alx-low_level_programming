7dedb0f7737f91d6c961b3b3bd292af53da614bd
codingbelhaous <coding.belhaous@gmail.com>
2023-07-01 12:41:01 -0700

    print character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 -  that prints every other character of a string, starting with the first character
+ * puts2 - function that print out every other charactre
  *
  * @str: the string to be print out
  *
