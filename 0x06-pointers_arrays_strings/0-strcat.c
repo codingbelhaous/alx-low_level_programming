09aabc0070cba92d982f9e78e04c3cd6c85af565
codingbelhaous <coding.belhaous@gmail.com>
2023-07-01 13:22:28 -0700

    strcqt

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - a function that concatenates two strings
+ *
+ * @dest: string
+ * @src: string
+ * @n: number
+ *
+ * Return: character
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i])
+		i++;
+	while (src[j] && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
