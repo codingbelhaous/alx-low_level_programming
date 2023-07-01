07a11c231c3c6d071fe7a8f49b279da99c407e41
codingbelhaous <coding.belhaous@gmail.com>
2023-07-01 13:29:53 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,13 +10,15 @@
  * Return: character
  */
 
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
 	int i = 0;
 	int j = 0;
 
 	while (dest[i])
+	{
 		i++;
+	}
 	while (src[j] && j < n)
 	{
 		dest[i] = src[j];
